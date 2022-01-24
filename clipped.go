0113a529f422f2cdbd8a261d312389c1abccb9c2
Henrique Bento <algarvio.henrique@gmail.com>
2022-01-24 15:50:41 +0000

    reading user data

---
+++ clipped.go
@@ -0,0 +1,6 @@
+package main
+
+type User struct {
+	Name     string `json: "name"`
+	Password string `json: "password"`
+}
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+const (
+	defaultPath = "info.json"
+)
+
+func main() {
+	u := &User{}
+	if err := getInfoFromFile(u, defaultPath); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getInfoFromFile(u *User, path string) error {
+	info, err := os.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("Error reading from json: %w", err)
+	}
+
+	if err := json.Unmarshal(info, u); err != nil {
+		return fmt.Errorf("Error reading from json: %w", err)
+	}
+
+	return nil
+}
